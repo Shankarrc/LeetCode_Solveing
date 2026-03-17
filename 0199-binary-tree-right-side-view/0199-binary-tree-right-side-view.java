fb8ffa8160a9d0e7a516b2b184785be3928ad073
Shankar <168357309+Shankarrc@users.noreply.github.com>
2026-03-18 00:13:52 +0530

    Time: 0 ms (100%), Space: 43.7 MB (45.28%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,33 +14,47 @@
  * }
  */
 class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
-        Queue<TreeNode>q=new LinkedList<>();
-        ArrayList<Integer>ans=new ArrayList<>();
+    public void dfs(TreeNode root,int level,List<Integer>res ){
 
-        if(root==null) return ans;
+        if(root ==null) return;
+        if(level==res.size()){
+        res.add(root.val);
 
-        q.add(root);
-
-        while(!q.isEmpty()){
-
-            int sz=q.size();
-
-            for(int i=0;i<sz;i++){
-                TreeNode curr=q.poll();
-
-                if(i==sz-1)ans.add(curr.val);
-                
-                
-                if(curr.left != null){
-                    q.offer(curr.left);
-                }
-                if(curr.right !=null){
-                    q.offer(curr.right);
-                }
-
-            }
         }
-        return ans;
+        dfs(root.right,level+1,res);
+        dfs(root.left,level+1,res);
+    }
+    public List<Integer> rightSideView(TreeNode root) {
+        // Queue<TreeNode>q=new LinkedList<>();
+        // ArrayList<Integer>ans=new ArrayList<>();
+
+        // if(root==null) return ans;
+
+        // q.add(root);
+
+        // while(!q.isEmpty()){
+
+        //     int sz=q.size();
+
+        //     for(int i=0;i<sz;i++){
+        //         TreeNode curr=q.poll();
+
+        //         if(i==sz-1)ans.add(curr.val);
+                
+                
+        //         if(curr.left != null){
+        //             q.offer(curr.left);
+        //         }
+        //         if(curr.right !=null){
+        //             q.offer(curr.right);
+        //         }
+
+        //     }
+        // }
+        // return ans;
+
+        List<Integer>res=new ArrayList< >();
+        dfs(root,0,res);
+        return res;
     }
 }
