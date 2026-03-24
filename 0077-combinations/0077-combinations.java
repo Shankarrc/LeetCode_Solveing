0b0ff51f2d1a30f7fe6279f8a534d8fbc76e570e
Shankar <168357309+Shankarrc@users.noreply.github.com>
2026-03-24 11:36:44 +0530

    Time: 46 ms (5.09%), Space: 101.2 MB (5.1%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,26 +1,24 @@
 class Solution {
-    List<List<Integer>>ans=new ArrayList<>();
-    
-    public void fun(int i,int n,int k,List<Integer>shan)
+    public List<List<Integer>> fun(int i,int n,int k,List<Integer>ans){
+        List<List<Integer>>res=new ArrayList<>();
 
-    {
-        if(k==shan.size()){
-            ans.add(new ArrayList<>(shan));
-             
-            return;
+        if(k==ans.size()){
+            res.add(new ArrayList<>(ans));
+            return res;
         }
-        if(i>n)
-        {
-            return ;
+
+        if(i>n){
+            return res;
         }
-        shan.add(i);
-        fun(i+1,n,k,shan);
-        shan.remove(shan.size()-1);
-        fun(i+1,n,k,shan);
+
+        ans.add(i);
+        res.addAll(fun(i+1,n,k,ans));
+        ans.remove(ans.size()-1);
+        res.addAll(fun(i+1,n,k,ans));
+        return res;
     }
     public List<List<Integer>> combine(int n, int k) {
-       List<Integer> shan = new ArrayList<>();
-        fun(1,n,k,  shan);
-        return ans;
+        
+        return fun(1,n,k,new ArrayList<>());
     }
 }
