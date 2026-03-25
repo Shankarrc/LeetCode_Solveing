d7c9856e1fa59598fc29e8cec32c99755dfc8bf6
Shankar <168357309+Shankarrc@users.noreply.github.com>
2026-03-25 07:40:56 +0530

    Time: 0 ms (100%), Space: 116.1 MB (74.98%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
@@ -3,26 +3,25 @@ class Solution {
         int n=mat.length;
         int m=mat[0].length;
 
-    int low=0,high=m-1;
-
-    while(low<=high){
-
-        int mid=(low+high)/2;
-
+        int low=0,high=m-1;
         int maxrow=0;
+        while(low<=high){
+
+            int mid=(low+high)/2;
+        
         for(int i=0;i<n;i++){
 
             if(mat[i][mid]>mat[maxrow][mid]){
                 maxrow=i;
             }
+
         }
 
-        int left=(mid-1>=0)?mat[maxrow][mid-1]:-1;
-        int right=(mid+1<m)?mat[maxrow][mid+1]:-1;
+        int left=(mid-1==-1)?-1:mat[maxrow][mid-1];
+        int right=(mid+1>=m)?-1:mat[maxrow][mid+1];
 
-        //check peak element left and right
-        if( mat[maxrow][mid]>left && mat[maxrow][mid]>right){
-            return new int []{maxrow,mid};
+        if(mat[maxrow][mid]>left && mat[maxrow][mid]>right){
+            return new int[]{maxrow,mid};
         }
         else if(left>mat[maxrow][mid]){
             high=mid-1;
@@ -30,8 +29,8 @@ class Solution {
         else{
             low=mid+1;
         }
-
-    }
-return new int[]{-1,-1};
+            
+        }
+        return new int[]{-1,-1};
     }
 }
