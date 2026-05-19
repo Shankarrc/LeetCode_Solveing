1792c59aaf1d1e7005d382f51de2cfdeab9bfc76
Shankar <168357309+Shankarrc@users.noreply.github.com>
2026-05-19 20:39:49 +0530

    Time: 87 ms (8.91%), Space: 78.1 MB (82.58%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.java
+++ 1345-jump-game-iv/1345-jump-game-iv.java
@@ -1,20 +1,16 @@
 class Solution {
     public int minJumps(int[] arr) {
-
-        int n=arr.length;
         
-        HashMap<Integer,List<Integer>> shan=new HashMap<>();
+        HashMap<Integer,List<Integer>>shan=new HashMap<>();
 
-        for(int i=0;i<n;i++){
-
-            shan.computeIfAbsent(arr[i],k->new ArrayList<>())
-                .add(i);
-                  
+        for(int i=0;i<arr.length;i++){
+            shan.computeIfAbsent(arr[i],k->new ArrayList<>()).add(i);
         }
-        Queue<Integer>q=new LinkedList<>();
 
+        Queue<Integer>q=new LinkedList<>();
+        int n=arr.length;
         q.add(0);
-        boolean vis[]=new boolean[arr.length];
+        boolean[]vis=new boolean[n];
         vis[0]=true;
         int step=0;
         while(!q.isEmpty()){
@@ -22,21 +18,21 @@ class Solution {
             int size=q.size();
 
             while(size-->0){
-               int i=q.poll();
 
-                if(i==n-1) return step;
-                List<Integer>next=shan.get(arr[i]);
-                next.add(i-1);
-                next.add(i+1);
-                for(int a:next){
+                int i=q.poll();
 
-                    if(  a>=0 && a<n &&!vis[a]){
-                        q.add(a);
-                        vis[a]=true;
+                if(i==n-1) return  step;
+
+                List<Integer> nextval=shan.get(arr[i]);
+                nextval.add(i-1);
+                nextval.add(i+1);
+                for(int val:nextval){
+
+                    if(val>=0 && val<n && !vis[val]){
+                        vis[val]=true;
+                        q.add(val);
                     }
                 }
-
-
                 shan.get(arr[i]).clear();
             }
             step++;
