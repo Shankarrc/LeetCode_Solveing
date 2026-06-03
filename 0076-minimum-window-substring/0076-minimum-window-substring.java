cd30111b67dd1d4f8f7cdd943584dda982761f30
Shankar <168357309+Shankarrc@users.noreply.github.com>
2026-06-03 20:48:22 +0530

    Time: 17 ms (52.64%), Space: 47.1 MB (50.24%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,52 +1,46 @@
 class Solution {
     public String minWindow(String s, String t) {
-        
         HashMap<Character,Integer>shan=new HashMap<>();
 
+        int n=t.length();
         for(int i=0;i<t.length();i++){
             char ch=t.charAt(i);
             shan.put(ch,shan.getOrDefault(ch,0)+1);
         }
-
-        int l=0,r=0;
+        int r=0;
+        int l=0;
+        int start=-1;
+        int minlen=Integer.MAX_VALUE;
         int cnt=0;
-        int m=t.length();
-        int minlen=Integer.MAX_VALUE,str=-1;
-
 
         while(r<s.length()){
-            
             char ch=s.charAt(r);
+            if(shan.containsKey(ch)){
 
-            if(shan.containsKey(ch) ){
-                if( shan.get(ch)>0)cnt++;
+                 if(shan.get(ch)>0){
+                cnt++;
+            }
                 shan.put(ch,shan.get(ch)-1);
             }
-
-            while(cnt==m){
-
-
+           
+            while(cnt==n){
 
                 if((r-l+1)<minlen){
                     minlen=r-l+1;
-                    str=l;
+                    start=l;
                 }
-
                 char c=s.charAt(l);
-
                 if(shan.containsKey(c)){
+                shan.put(c,shan.get(c)+1);
 
-                    shan.put(c,shan.get(c)+1);
+                if(shan.get(c)>0) cnt--;
 
-                    if(shan.get(c)>0)cnt--;
                 }
-
                 l++;
 
             }
-
             r++;
         }
-        return str==-1?"":s.substring(str,str+minlen);
+        return start==-1?"":s.substring(start,start+minlen);
     }
 }
